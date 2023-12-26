378e51e32095624833bdec07b03f37a4498b6c10
Vladyslavi <135136418+Vladyslavi@users.noreply.github.com>
2023-12-26 19:02:44 +0200

    commit

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Loader from 'components/Loader/Loader';
 import EditorList from 'pages/EditorList/EditorList';
 import Form from 'components/Form/Form';
+import { useSearchParams } from 'react-router-dom';
 import { fetchSearchByKeyword } from 'services/TmbdApi';
 
 const Movies = () => {
     const [searchFilms, setSearchFilms] = useState([]);
     const [loading, setLoading] = useState(false);
     const [noMoviesText, setNoMoviesText] = useState(false);
+    const [searchParams] = useSearchParams();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const storedQuery = searchParams.get('query');
+            if (storedQuery) {
+                try {
+                    setLoading(true);
+                    const searchResults = await fetchSearchByKeyword(storedQuery);
+                    setSearchFilms(searchResults);
+                    setNoMoviesText(searchResults.length === 0);
+                } catch (error) {
+                    console.log(error);
+                } finally {
+                    setLoading(false);
+                }
+            }
+        };
+        fetchData();
+        return () => {
+        };
+    }, [searchParams]);
 
     const searchMovies = queryMovie => {
         setLoading(true);
@@ -30,11 +53,11 @@ const Movies = () => {
             <Form searchMovies={searchMovies} />
             {loading && <Loader />}
             {noMoviesText && (
-                <p>There is no movies with this request. Please, try again</p>
+                <p>There is no movie with this request. Please try again.</p>
             )}
             {searchFilms && <EditorList films={searchFilms} />}
         </main>
     );
 };
 
-export default Movies;                                                                                                                                                         
+export default Movies;
